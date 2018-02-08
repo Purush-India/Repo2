47380770eb56d748b8ccf6aaf5fca362a039a017
purush <invensis.bangalore@gmail.com>
2018-02-08 15:32:42 +0530

    Commit1

---
+++ TestProject/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void OpenGoogle()
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.Quit();
+           
+        }
+    }
+}
